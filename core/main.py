adfddc231fd3bf2a9dd4f903d29d6ec5ca1d7388
Jan Slováček <honza.slovacek@seznam.cz>
2016-10-15 01:17:29 +0200

    Implementation of 'Hello World' web server in Bottle framework

--- core/main.py
+++ core/main.py
@@ -1,5 +1,6 @@
 from module.DatabaseModule import DatabaseModule
 from module.Helper import IO
+from module import web_server
 
 
 def main():
@@ -7,6 +8,8 @@ def main():
     database = DatabaseModule()
 
     IO.print_to_json("data.json", database.get_records())
+
+    web_server.run_server()
 
 if __name__ == '__main__':
     main()

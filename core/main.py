58179f760aa4c53582c9481d30d7ceb166bf06c6
MichalBeder <xbeder00@stud.fit.vutbr.cz>
2016-10-21 01:00:20 +0200

    test LED blinking

--- core/main.py
+++ core/main.py
@@ -1,7 +1,7 @@
 from module.DatabaseModule import DatabaseModule
 from module.Helper import IO
 from module import web_server
-
+from module import sensor_DHT11
 
 def main():
     print ("Hello, SEN!")
@@ -9,7 +9,8 @@ def main():
 
     IO.print_to_json("data.json", database.get_records())
 
-    web_server.run_server()
+    #web_server.run_server()
+    sensor_DHT11.blink()
 
 if __name__ == '__main__':
     main()

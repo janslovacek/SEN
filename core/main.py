d4ad9b7c452b7a05db610a0fc556008f6e022701
MichalBeder <xbeder00@stud.fit.vutbr.cz>
2016-10-26 00:16:18 +0200

    Removed old import. In main function commented HW function. In general HW functions should not be called in other branches than HW yet.

--- core/main.py
+++ core/main.py
@@ -1,17 +1,13 @@
 from database_module import DatabaseModule
 from helper import IO
-import web_server
 import sensor_DHT11
-
 
 def main():
     print ("Hello, SEN!")
     database = DatabaseModule()
     #IO.print_to_json("data.json", database.get_records())
-
     #web_server.run_server()
-    sensor_DHT11.blink()
-
+    #sensor_DHT11.blink()
 
 if __name__ == '__main__':
     main()

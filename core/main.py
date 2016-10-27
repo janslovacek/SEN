27e5220d51502eb4f2eeb6af8530c18d4f0e5aa1
Jan Paulovcak <janpaulovcak@gmail.com>
2016-10-27 14:39:41 +0200

    Extended database with pressure and rgwb values, added possibility to sort records, added last row fetch and getting average values

--- core/main.py
+++ core/main.py
@@ -1,11 +1,14 @@
 from database_module import DatabaseModule
+from database_module import Sort
 from helper import IO
 import sensor_DHT11
 
+
 def main():
-    print ("Hello, SEN!")
+    print("Hello, SEN!")
     database = DatabaseModule()
-    #IO.print_to_json("data.json", database.get_records())
+    # print(database.get_records(5, 0, Sort.DESC))
+    # IO.print_to_json("data.json", database.get_records())
     #web_server.run_server()
     #sensor_DHT11.blink()
 
